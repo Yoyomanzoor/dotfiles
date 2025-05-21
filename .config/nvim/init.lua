13f51dbcb8c551773a9be8ae4e80ad3fb5f894a7
yoyomanzoor-arch-x1 <smanzoor@umich.edu>
2025-05-20 23:57:03 -0400

    nvim updates: markdown preview and treesitter keybinds
    treesitter context selection now works with keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1234,6 +1234,15 @@ require("lazy").setup({
 				additional_vim_regex_highlighting = { "ruby" },
 			},
 			indent = { enable = true, disable = { "ruby" } },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<leader><CR>",
+					scope_incremental = "<leader><CR>",
+					node_incremental = "<leader><TAB>",
+					node_decremental = "<leader><BS>",
+				},
+			},
 		},
 		-- There are additional nvim-treesitter modules that you can use to interact
 		-- with nvim-treesitter. You should go explore a few and see what interests you:
---
+++ .config/nvim/lua/custom/plugins/renpy.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"chaimleib/vim-renpy",
+		-- config = function()
+		-- 	vim.api.nvim_create_autocmd("FileType", {
+		-- 		pattern = "rpy",
+		-- 		callback = function()
+		-- 			vim.opt_local.commentstring = "#%s"
+		-- 		end,
+		-- 	})
+		-- end,
+	},
+}
