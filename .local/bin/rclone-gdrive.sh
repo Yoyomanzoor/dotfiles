22847b2eb359e79d8c2ace261b991bf7752748a9
yoyomanzoor-arch-x1 <smanzoor@umich.edu>
2025-07-15 14:54:03 -0400

    lots more scripts, for gdrive and syncing news

--- .local/bin/rclone-gdrive.sh
+++ .local/bin/rclone-gdrive.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/bash
 
+REMOTE_NAME=""
+
+REMOTES=$(rclone listremotes)
+
+if echo "$REMOTES" | grep -q "googledrive"; then
+	REMOTE_NAME="googledrive"
+elif echo "$REMOTES" | grep -q "gdrive"; then
+	REMOTE_NAME="gdrive"
+elif echo "$REMOTES" | grep -q "drive"; then
+	REMOTE_NAME="drive"
+elif echo "$REMOTES" | grep -q "remote"; then
+	REMOTE_NAME="remote"
+else
+	echo "No Google Drive remote found. Please set up a remote using rclone."
+	exit 1
+fi
+
 mkdir -p $HOME/drive
-rclone mount remote: $HOME/drive/
+rclone mount "$REMOTE_NAME": $HOME/drive/
