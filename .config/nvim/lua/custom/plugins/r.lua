5f4f4528527f1e946bd1e489612930fbdbcc38a9
YoyoMSIArch <smanzoor@umich.edu>
2026-02-10 17:16:40 -0500

    r nvim

--- .config/nvim/lua/custom/plugins/r.lua
+++ .config/nvim/lua/custom/plugins/r.lua
@@ -3,10 +3,12 @@ return {
 		"R-nvim/R.nvim",
 		-- Only required if you also set defaults.lazy = true
 		lazy = false,
-		-- R.nvim is still young and we may make some breaking changes from time
-		-- to time. For now we recommend pinning to the latest minor version
-		-- like so:
-		version = "~0.1.0",
+		config = function()
+			require("r").setup({
+				R_app = "radian",
+				disable_cmds = { "RInsertPipe" },
+			})
+		end,
 	},
 	{
 		"chrisbra/csv.vim",
